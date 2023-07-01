4916f650a9d618d1c365ad946ec3f82e50ec1344
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-07-02 00:25:17 +0300

    01 Bus stop
    update

--- 06-Exercise Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ 06-Exercise Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,4 +1,4 @@
-function getInfo() {
+async function getInfo() {
     // read  input 
     // request to server
     //parse data
@@ -6,7 +6,34 @@ function getInfo() {
     //check error
 
     const stopId = document.getElementById('stopId').value;
-    const url = ` http://localhost:3030/jsonstore/bus/businfo/${busId}`;
+    const stopNameElement= document.getElementById('stopName');
+    const timeTableElement = document.getElementById('buses')
+    const url = ` http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    
+    //  console.log(data);
+    try {
+        stopNameElement.textContent = 'Loading...';
+        timeTableElement.replaceChildren()
+      const res = await fetch(url);
+      if(res.status !== 200){
+        alert('bust stop not faound')
+      }
+      const data = await res.json();
+
+      //Object.entries(data.buses)
+
+      stopNameElement.textContent = data.name
+
+    Object.entries(data.buses).forEach(b => {
+        let liElement = document.createElement('li');
+        liElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+        timeTableElement.appendChild(liElement)
+    })
+    
+  } catch (error) {
+    
+  }
 
 
 }

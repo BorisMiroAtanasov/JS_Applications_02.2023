025a81a8cd9d7f71db6c8ef875386d86218fe9d3
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-07-01 23:51:15 +0300

    01.Bus stop
    Update

---
+++ 06-Exercise Asynchronous Programming/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -0,0 +1,12 @@
+function getInfo() {
+    // read  input 
+    // request to server
+    //parse data
+    //dysplay data
+    //check error
+
+    const stopId = document.getElementById('stopId').value;
+    const url = ` http://localhost:3030/jsonstore/bus/businfo/${busId}`;
+
+
+}

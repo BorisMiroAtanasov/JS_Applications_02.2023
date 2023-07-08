f0b735b47a169231242165d8c402acc75d6962da
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-07-09 00:57:31 +0300

    01 Massanger
    done 100%

--- 08-Data-and-Authentication-Exercise/01.Messenger/app.js
+++ 08-Data-and-Authentication-Exercise/01.Messenger/app.js
@@ -1,5 +1,45 @@
+const url = `http://localhost:3030/jsonstore/messenger`;
+const massages = document.getElementById("messages");
+
 function attachEvents() {
-    console.log('TODO...');
+  document.getElementById("submit").addEventListener("click", postMassages);
+  document.getElementById("refresh").addEventListener("click", loadAllMassages);
+}
+async function postMassages() {
+    const author = document.querySelector('[name="author"]');
+    const content = document.querySelector('[name="content"]');
+    if(author.value !== "" || content.value !== ""){
+        await request(url , {author:author.value, content: content.value})
+    author.value = "";
+    content.value =""
+    };
+
+    
+
+}
+
+async function loadAllMassages() {
+  const responsse = await fetch(url);
+  const data = await responsse.json();
+
+  massages.value = Object.values(data)
+    .map(({ author, content }) => `${author}: ${content}`)
+    .join("\n");
+}
+async function request(ulr, option) {
+  if (option) {
+    option = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(option),
+    };
+  }
+  const response = await fetch(ulr, option);
+
+  return response.json()
+
 }
 
 attachEvents();

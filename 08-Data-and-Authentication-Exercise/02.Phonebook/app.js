c603be5ac1bd2db600e14b742fa45a8ff2cf8a3d
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-07-09 23:53:59 +0300

    Update

--- 08-Data-and-Authentication-Exercise/02.Phonebook/app.js
+++ 08-Data-and-Authentication-Exercise/02.Phonebook/app.js
@@ -1,5 +1,66 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+  const url = `http://localhost:3030/jsonstore/phonebook`;
+
+  const ul = document.getElementById("phonebook");
+  const loadBtn = document.getElementById("btnLoad");
+  const createBtn = document.getElementById("btnCreate");
+  const person = document.getElementById("person");
+  const phone = document.getElementById("phone");
+
+  loadBtn.addEventListener("click", onClickLoad);
+  createBtn.addEventListener("click", onClickCreate);
+
+  async function onClickLoad() {
+    ul.innerHTML = "";
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    //console.log(data);
+
+    Object.values(data).forEach((x) => {
+      const { person, phone, _id } = x;
+      const li = createEle("li", `${person}: ${phone}`, ul);
+      li.setAttribute("id", _id);
+
+      const deleteBtn = createEle("button", "Delete", li);
+      deleteBtn.setAttribute = ("id", "btnDelete");
+      deleteBtn.addEventListener('click', onClickDelete)
+    });
+  }
+  async function onClickDelete(ev){
+    const id = ev.target.parentNode.id;
+    ev.target.parentNode.remove()
+    //console.log(id);
+    const deleteResponse = await fetch(`${url}/${id}`,{
+        method: 'DELETE'
+    })
+
+    
+  };
+
+  function createEle(type, text, appender) {
+    const result = document.createElement(type);
+    result.textContent = text;
+    appender.appendChild(result);
+
+    return result;
+  };
+
+  async function onClickCreate() {
+    if (person.value !== "" && phone.value !== "") {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ person: person.value, phone: phone.value }),
+      });
+      ul.innerHTML = "";
+      loadBtn.click();
+      person.value == "";
+      phone.value == "";
+    }
+  }
+};
+
 
 attachEvents();

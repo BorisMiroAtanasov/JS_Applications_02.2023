c44672c70781e5e5627cbb7fbbcd04ac39acf572
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-07-14 23:46:17 +0300

    update

--- 09- JS-Applications-Architecture-and-Testing/softuni-autoparts-demo-master/src/login.js
+++ 09- JS-Applications-Architecture-and-Testing/softuni-autoparts-demo-master/src/login.js
@@ -1,25 +1,24 @@
 import { post } from '../data/api.js';
 import { login } from '../data/auth.js';
 import { showHome } from './home.js';
+import { createSubmitListener } from './util.js';
+
 
 const loginSection = document.getElementById('login');
 
 const loginForm = loginSection.querySelector('#login-form');
-loginForm.addEventListener('submit', onLogin);
+createSubmitListener (loginForm, onLogin  )
+
+
 
 export function showLogin() {
     document.querySelector('main').replaceChildren(loginSection);
 }
 
-async function onLogin(event) {
-    event.preventDefault();
-    const formData = new FormData(loginForm);
-    const { email, password } = Object.fromEntries(formData.entries());
+async function onLogin(email, password) {
 
   // const userData =  await sendLoginData(email, password);
   await login(email, password)
-
-   
 
    loginForm.reset();
 
---
+++ 09- JS-Applications-Architecture-and-Testing/softuni-autoparts-demo-master/src/util.js
@@ -0,0 +1,14 @@
+export function createSubmitListener(form , callback){
+    form.addEventListener('submit', onSubmit);
+
+    function onSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+
+        callback(data)
+
+
+    }
+
+}

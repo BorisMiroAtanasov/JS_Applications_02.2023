92b254bab442641945e2f8fe09fd2c59710207b2
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-08-05 22:48:29 +0300

    new

---
+++ Exam Prep/Exam prep - 21.03.23 - Copy/src/app.js
@@ -0,0 +1,51 @@
+import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js';
+import { getUserData } from './util.js';
+import { layoutTemplate } from './view/layout.js';
+import { homePage } from './view/home.js';
+import { loginPage } from './view/login.js';
+import { registerPage } from './view/register.js';
+import { logout } from './data/auth.js';
+import { catalogPage } from './view/catalog.js';
+import { detailsPage } from './view/details.js';
+import { createPage } from './view/create.js';
+import { editPage } from './view/edit.js';
+
+import * as api from './data/applications.js';
+window.api = api
+
+
+//To do change render root depending on project HTML structure
+
+const root = document.getElementById('wrapper');
+page(decorateContext)
+page ('index.html' , '/');
+page('/', homePage);
+page('/catalog', catalogPage);
+page('/catalog/:id', detailsPage);
+page('/catalog/:id/edit', editPage);
+page('/create', createPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/logout', logoutAction);
+
+
+
+page.start();
+
+function decorateContext(ctx, next){
+ctx.render = renderView
+
+    next();
+}
+//To Do injeckt dependensis
+function renderView(content){
+    const userData = getUserData();
+    render(layoutTemplate(userData,content), root)
+
+}
+
+function logoutAction(ctx){
+    logout();
+    ctx.page.redirect('/');
+}
---
+++ Exam Prep/Exam prep - 21.03.23 - Copy/src/view/layout.js
@@ -0,0 +1,32 @@
+//import { html } from "../../node_modules/lit-html/lit-html.js"
+
+import { html } from '../../node_modules/lit-html/lit-html.js'
+
+// To Do replacewith actual layuot
+export const layoutTemplate = (userData, content) => html `
+  <header>
+        <!-- Navigation -->
+        <a id="logo" href="/"
+          ><img id="logo-img" src="./images/logo.jpg" alt=""
+        /></a>
+
+        <nav>
+          <div>
+            <a href="/catalog">Dashboard</a>
+          </div>
+
+          ${userData ? html `<div class="user">
+            <a href="/create">Create Offer</a>
+            <a href="logout">Logout</a>
+          </div>` : html `<div class="guest">
+            <a href="/login">Login</a>
+            <a href="register">Register</a>
+          </div>`}
+
+      
+          
+        </nav>
+      </header>
+      <main>
+        ${content}
+      </main>`
